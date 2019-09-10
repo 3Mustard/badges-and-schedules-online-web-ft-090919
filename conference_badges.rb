78eae6d3ba1e11b97f5495a91ac813973e0e7969
Matthew C <m23.cullen@gmail.com>
2019-09-10 14:15:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,5 +13,8 @@ end
 
 def assign_rooms(speakers)
   room_assignments = []
-  speakers.eah
+  speakers.each_with_index do |name,index|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+  room_assignments
 end
